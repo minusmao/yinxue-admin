0ff18ea471d7a05ccff1d88c079a7da97b1a3bad
minus <trice12345@aliyun.com>
2023-09-03 02:09:56 +0800

    feat: 配置并测试 gateway 网关

---
+++ yinxue-category/src/main/java/com/example/controller/DemoController.java
@@ -0,0 +1,24 @@
+package com.example.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 测试示例
+ * @author minus
+ * @since 2023-09-02 18:17
+ */
+@RestController
+@RequestMapping("/demo")
+@Slf4j
+public class DemoController {
+
+    @GetMapping
+    public String demo() {
+        log.info("category demo");
+        return "category demo";
+    }
+
+}
--- yinxue-gateway/src/main/java/com/example/AdminGatewayApplication.java
+++ yinxue-gateway/src/main/java/com/example/AdminGatewayApplication.java
@@ -1,13 +1,17 @@
 package com.example;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
 /**
  * 网关模块
  *
  * @author minus
  * @since 2023-09-02 18:17
  */
+@SpringBootApplication
 public class AdminGatewayApplication {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SpringApplication.run(AdminGatewayApplication.class, args);
     }
 }
